301a4045973a48c6ebb17b4293798b20a1c088ad
Andrea <roundcrisis@gmail.com>
2014-12-15 08:59:13 +0000

    complete solution

---
+++ InTheKeyOfCSharp/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenTK.Audio;
+using OpenTK.Audio.OpenAL;
+
+namespace InTheKeyOfCSharp
+{
+	public class SoundStuff
+	{
+		private const int SamplingFrequency = 44100;
+
+		private Dictionary<string, float> _notes = new Dictionary<string, float>
+		{
+			{"C", 261.626f},
+			{"C#", 277.183f},
+			{"D", 293.665f},
+			{"D#", 311.127f},
+			{"E", 329.628f},
+			{"F", 349.228f},
+			{"F#", 369.994f},
+			{"G", 391.995f},
+			{"G#", 415.305f},
+			{"A", 440f},
+			{"A#", 466.164f},
+			{"B", 493.883f},
+		}; 
+		private AudioContext _audioContext;
+		private int _audioSourceIndex;
+		private int _buffer;
+
+		private string _song = "F# F# G# F# B A#";
+
+		public SoundStuff()
+		{
+			_audioContext = new AudioContext();
+
+			_buffer = AL.GenBuffer();
+			_audioSourceIndex = AL.GenSource();
+
+			var notes = _song.Split(' ');
+			var data = new short[SamplingFrequency * notes.Length];
+			var dataOffset = 0;
+			var noteLength = 0.5f;
+			foreach (var note in notes)
+			{
+				var freq = _notes[note];
+				for (var i = 0; i < SamplingFrequency * noteLength; i++)
+				{
+					var factor = (2 * Math.PI * freq) / SamplingFrequency * i;
+					var sinf = Math.Sin(factor);
+
+					data[dataOffset + i] = (short)(sinf * short.MaxValue);
+				}
+
+				dataOffset += (int)(SamplingFrequency * noteLength);
+			}
+			
+			AL.BufferData(_buffer, ALFormat.Mono16, data, data.Length * 2, SamplingFrequency);
+			AL.Source(_audioSourceIndex, ALSourcei.Buffer, _buffer);
+			
+			AL.SourcePlay(_audioSourceIndex);
+			Console.WriteLine(AL.GetError());
+
+			Console.ReadLine();
+		}
+
+		public void Play(object song)
+		{
+
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var sound = new SoundStuff();
+		}
+	}
+}
---
+++ TwelveDaysOfXmasInTheKeyOfFSharp/Program.fs
@@ -0,0 +1,79 @@
+﻿// --------------------------------------------------------------------------------------
+// F# and games in celebration times
+// --------------------------------------------------------------------------------------
+// (c) Andrea Magnorsky 2014 {@silverspoon}
+// Distributed under the open-source MS-PL license
+// --------------------------------------------------------------------------------------
+#if INTERACTIVE
+#r @"C:\source\TwelveDaysOfXmasInTheKeyOfFSharp\packages\OpenTKWithOpenAL.1.1.1589.5942\lib\NET40\OpenTK.dll"
+#load @"C:\source\TwelveDaysOfXmasInTheKeyOfFSharp\packages\FSharp.Charting.0.90.9\FSharp.Charting.fsx"
+#endif
+
+open System
+open OpenTK.Audio
+open OpenTK.Audio.OpenAL    
+open FSharp.Charting
+
+    let toFrequency (note:string) =
+        match note.ToUpper() with
+        | "C" ->  261.626f
+        | "C#" -> 277.183f
+        | "D"-> 293.665f
+        |"D#"-> 311.127f
+        |"E"-> 329.628f
+        |"F"-> 349.228f
+        |"F#"-> 369.994f
+        |"G"-> 391.995f
+        |"G#"-> 415.305f
+        |"A"-> 440.0f
+        |"A#"-> 466.164f
+        |"B"-> 493.883f
+        |_ ->  369.994f (*Defaults to F# because xMas ;) *)
+
+    let samplingFrequency = 44100.0
+    
+    let generateNote (freq:float32) =
+        let noteLength = 0.5
+        let seqLength = int  (samplingFrequency * noteLength)
+        Seq.init seqLength (fun i ->                                 
+                        (2.0 * Math.PI * float freq) / samplingFrequency * float i
+                        |> Math.Sin                        
+                        |> ( * )  (float Int16.MaxValue)
+                        |> int16
+                        )
+
+    let playSong (song: string) =
+        let notes = song.Split ' '
+
+        use audioContext = new AudioContext()
+        let buffer = AL.GenBuffer()
+        let audioSourceIndex = AL.GenSource()
+         
+        let freqToWaves = toFrequency >>  generateNote
+        let data = notes                        
+                        |> Seq.collect freqToWaves                        
+                        |> Array.ofSeq
+                        
+               
+        AL.BufferData (buffer, ALFormat.Mono16, data, data.Length * 2, int samplingFrequency)
+        AL.Source(audioSourceIndex, ALSourcei.Buffer, buffer)
+        AL.SourcePlay(audioSourceIndex)
+
+        
+        Console.WriteLine("Errors: {0}", AL.GetError())
+        Console.ReadLine() |> ignore
+
+        // Clean up after ourselves 
+        AL.DeleteBuffer buffer
+        AL.DeleteSource audioSourceIndex
+
+        ()
+
+let mysterySong = "F# F# G# F# B A#"
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv    
+    let silentNight = "F# F# G# F# F# D# D# D# F# F# G# F# F# D# D# D#" 
+    playSong silentNight    
+    0 // return an integer exit code
